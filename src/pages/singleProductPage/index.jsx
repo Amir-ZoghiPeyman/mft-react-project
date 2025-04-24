586d86c55c86d684e142eb92d6fca4dbcd8f6243
Amir-ZoghiPeyman <zoghipeyman.amir@gmail.com>
2025-04-25 00:59:25 +0330

    some state management (cart) codes added

--- src/pages/singleProductPage/index.jsx
+++ src/pages/singleProductPage/index.jsx
@@ -1,17 +1,31 @@
 import { useParams } from "react-router-dom"
+import { useMemo } from "react";
 import useFetchSingleProduct from "../../requests/useFetchSingleProduct"
+import useCart from "../../store/useCart";
 
 export default function SingleProductPage() {
-    const { productId } = useParams();
 
+    const { productId } = useParams();
     const { data } = useFetchSingleProduct(productId);
+    const { products, addProduct } = useCart();
+
+    const productCount = useMemo(() => {
+        const foundIndex = products.findIndex((item) => item.id === productId);
+        if (foundIndex == -1) {
+            return 0;
+        } else {
+            return products[foundIndex].quantity;
+        }
+    }, [products]);
 
     return (
-        <>
-            <div>
-                <img src={data?.data?.image} alt="" />
-                <h2>{data?.data?.title}</h2>
-            </div>
-        </>
-    )
+        <div>
+            <img src={data?.data?.image} alt="" />
+            <h1>{data?.data.title}</h1>
+
+            <button onClick={() => addProduct(productId)}>+</button>
+            <span>{productCount}</span>
+            <button>-</button>
+        </div>
+    );
 }
