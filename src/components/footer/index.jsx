10b0f3ac82333db43e7e6b00b6655b2096852b45
Amir-ZoghiPeyman <zoghipeyman.amir@gmail.com>
2025-05-09 10:18:42 +0330

    footer comp updated

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12">
                     <div className="space-y-4">
-                        <h4 className="text-lg font-semibold text-white mb-4">Navigations</h4>
+                        <h4 className="text-lg font-semibold text-white my-4 md:mt-0">Navigation</h4>
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                             {[
                                 'Sell online',
@@ -24,8 +24,7 @@ export default function Footer() {
                                 <a
                                     key={item}
                                     href="#"
-                                    className="text-sm hover:text-blue-400 transition-colors"
-                                >
+                                    className="text-sm hover:text-blue-400 transition-colors">
                                     {item}
                                 </a>
                             ))}
